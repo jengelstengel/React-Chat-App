61f3e7598efdb3050a6ac9b62f1c4577d349f25d
jengelstengel <twan.mulder@gmail.com>
2018-07-02 13:17:15 +0200

    init commit
    init commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/412d9e07-5559-4cd1-b2b8-d6ce2809ecea/token";
+const instanceLocator = "v1:us1:412d9e07-5559-4cd1-b2b8-d6ce2809ecea";
+
+export { tokenUrl, instanceLocator }
